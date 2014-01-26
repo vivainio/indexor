e508ab57084e5e01f0d417c4cfc915d827ef3c18
Ville M. Vainio <vivainio@gmail.com>
2014-01-26 17:24:08 +0200

    visit

--- indexor.go
+++ indexor.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"path/filepath"
+	"fmt"
+	"flag"
+	"os"
+)
 
-import "fmt"
+func visit(path string, f os.FileInfo, err error) error {
+	fmt.Printf("Visited: %s\n", path)
+	return nil
 
+
+}
 func main() {
 	fmt.Printf("Hello, world.\n")
+	flag.Parse()
+	root := flag.Arg(0)
+	err:=filepath.Walk(root, visit)
+	fmt.Printf("filepath.Walk() returned %v\n", err)	
 }
 
 
